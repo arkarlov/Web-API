eac7581c0c1499c3c51cbfbd26b3c1f82e5fafb7
Artem Karlov <ar.karlov@gmail.com>
2024-07-27 23:36:20 +0200

    add speech recognition

--- script.js
+++ script.js
@@ -24,3 +24,58 @@ const handleSpeechElement = (event) => {
 Array.from(speechContainer.children).forEach((child) => {
   child.onclick = handleSpeechElement;
 });
+
+console.log("Hello SpeechRecognition");
+
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const SpeechGrammarList =
+  window.SpeechGrammarList || window.webkitSpeechGrammarList;
+const SpeechRecognitionEvent =
+  window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
+
+const resultContainer = document.getElementById("speech-container");
+let p = null;
+
+const recognition = new SpeechRecognition();
+// const speechRecognitionList = new SpeechGrammarList();
+
+recognition.continuous = false;
+recognition.lang = "en-US";
+recognition.interimResults = true;
+
+recognition.onstart = () => {
+  p = document.createElement("p");
+  resultContainer.appendChild(p);
+};
+
+recognition.onresult = (event) => {
+  const transcript = event.results[0][0].transcript;
+  console.log(`word: ${transcript}`);
+  console.log(`Confidence: ${event.results[0][0].confidence}`);
+
+  if (p) {
+    p.textContent = `${transcript.replace(/unicorn/gi, "🦄")}.`;
+  }
+
+  if (event.results[0].isFinal) {
+    console.log("It's a final result");
+    Array.from(speechContainer.children).forEach((child) => {
+      child.onclick = handleSpeechElement;
+    });
+  }
+};
+
+recognition.onspeechend = () => {
+  console.log("Stop recognition");
+  recognition.stop();
+};
+
+recognition.onnomatch = (event) => {
+  console.log("I didn't recognize that.");
+};
+
+document.getElementById("button-recognition").onclick = () => {
+  console.log("Start recognition");
+  recognition.start();
+};
